b36b762bbe0884a8ca733975857cdad294cd6de4
Danielrosalescorona <82550105+Danielrosalescorona@users.noreply.github.com>
2021-05-31 18:53:16 -0500

    Add files via upload

--- Programa6.c
+++ Programa6.c
@@ -5,42 +5,42 @@
 
 int main[]
 [
-int banco; //acomular//
-int monto; //monto de ahorrar por evento//
-int numdepositos; //cantidad de veces que fui al banco//
-char letra; //saber si queremos realizar otro desposito//
+     int banco; //acomular//
+     int monto; //monto de ahorrar por evento//
+     int numdepositos; //cantidad de veces que fui al banco//
+     char letra; //saber si queremos realizar otro desposito//
 
 
-//voy al banco//
-printf("\n\n monto a depositar");
-scanf("%d,&monto");
-banco= banco+monto;//acumulador//
-numdepositos = numdepositos +1;//contador//
+    //voy al banco//
+    printf("\n\n monto a depositar");
+    scanf("%d,&monto");
+    banco= banco+monto;//acumulador//
+    numdepositos = numdepositos +1;//contador//
 
-printf("\n\n dinero que tienes en el banco &f, banco");
-printf("\n\n numero de depositos &d, numdepositos");
+    printf("\n\n dinero que tienes en el banco &f, banco");
+    printf("\n\n numero de depositos &d, numdepositos");
 
-banco = 0;
-monto = 0;
-numdepositos = 0; 
-respuesta = "s"
+    banco = 0;
+    monto = 0;
+    numdepositos = 0; 
+    respuesta = "s"
 
-do
-{
-//voy al banco//
-letra = "s"
-printf("\n\n monto a depositar");
-scanf("%d,&monto");
-banco= banco+monto;//acumulador//
-numdepositos = numdepositos +1;//contador//
+    do
+    {
+       //voy al banco//
+       letra = "s"
+       printf("\n\n monto a depositar");
+       scanf("%d,&monto");
+       banco= banco+monto;//acumulador//
+       numdepositos = numdepositos +1;//contador//
 
-printf("¿quieres realizar otro deposito? (s/n)?");
-scanf("%s,&respuesta")
+       printf("¿quieres realizar otro deposito? (s/n)?");
+       scanf("%s,&respuesta")
 
-} while (respuesta "s");// condition while = mientras //
+    } while (respuesta "s");// condition while = mientras //
 
-printf("\n\n dinero que tienes en el banco &f, banco");
-printf("\n\n numero de depositos &d, numdepositos");
-return 0;
+    printf("\n\n dinero que tienes en el banco &f, banco");
+    printf("\n\n numero de depositos &d, numdepositos");
+    return 0;
 
 ]
